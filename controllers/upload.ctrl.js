6ab64306d525b8c73749e02dff81ffc97b45fc9b
Julien Jesionek <julien.jesionek@gmail.com>
2022-06-15 12:41:41 +0200

    retouche gestion de fichier

--- controllers/upload.ctrl.js
+++ controllers/upload.ctrl.js
@@ -1,6 +1,6 @@
 const UserModel = require('../models/user');
 const fs = require('fs')
-const { uploadErrors } = require('../utils/errors.utils');
+//const { uploadErrors } = require('../utils/errors.utils');
 
 exports.uploadProfil = (req, res) => {
     UserModel.findOne({ _id : req.params.id})
@@ -9,16 +9,25 @@ exports.uploadProfil = (req, res) => {
                 res.status(404).json({error: new Error('Image non trouvée !')});
               }
             const filename = upload.picture.split('/profil/')[1];
-            
-            fs.unlink(`./client/public/uploads/profil/${filename}`, (error) => {
-                console.log(error)
+            const defaultPicture = "default-user-pic.png";
+
+            if(filename.match(defaultPicture)){
                 UserModel.updateOne( { _id: req.body.posterId},
                     { $set : {picture: `http://localhost:${process.env.PORT_FRONT}/uploads/profil/` + req.file.filename }},
                     { new : true, upsert: true, setDefaultOnInsert: true }
                 ) 
                     .then(() => res.status(200).json({ message: 'Image supprimée !'}))
                     .catch(error => res.status(400).json({ error }));
-            });
+            } else {
+                fs.unlink(`./client/public/uploads/profil/${filename}`, () => {
+                    UserModel.updateOne( { _id: req.body.posterId},
+                        { $set : {picture: `http://localhost:${process.env.PORT_FRONT}/uploads/profil/` + req.file.filename }},
+                        { new : true, upsert: true, setDefaultOnInsert: true }
+                    ) 
+                        .then(() => res.status(200).json({ message: 'Image supprimée !'}))
+                        .catch(error => res.status(400).json({ error }));
+                });
+            }
         })
         .catch(error => {console.log(error)})
 }
